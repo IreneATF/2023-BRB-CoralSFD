7a1e0eacdd8e4a2821be17f9ab5a18e71322479e
IreneATF <ireneatf@hotmail.ca>
2023-06-08 23:51:19 -0400

    Completed elliptic integral numerical calculation for exact area ellipsoid

--- fct_exact_surfarea_ellipsoid.R
+++ fct_exact_surfarea_ellipsoid.R
@@ -5,19 +5,17 @@ a_ell_x <- function(a,b,c,pm = 0) {
   
   f_integrand <- function(t) {1/sqrt(1-k^2*sin(t)^2)}
   Fint <- integrate(f_integrand, 0, phi)
-  print(Fint)
+  # print(Fint)
+  Fnum <- Fint[[1]]
   
   e_integrand <- function(t) {sqrt(1-k^2*sin(t)^2)}
   Eint <- integrate(e_integrand, 0, phi)
-  print(Eint)
+  # print(Eint)
+  Enum <- Eint[[1]]
   
-  Enum <- Eint[1]
-  Enum
-  Fnum <- Fint[1]
-  Fnum
-  
-  # area_x <- (2*pi*c^2)+((2*pi*a*b)/sin(phi))*(Eint*sin(phi)^2+Fint*cos(phi)^2)
-  # area_x_f <- fraction*area_x
+  area_x <- (2*pi*c^2)+((2*pi*a*b)/sin(phi))*(Enum*sin(phi)^2+Fnum*cos(phi)^2)
+  cat("\n The surface area is", area_x, "cm2;")
+  #area_x_f <- fraction*area_x
 }
 
-a_ell_x(60,50,20)
+a_ell_x(80,20,5)
